7153453fd7d83b7857c949644befbfc40d938fd0
PronSiman <Alex_vlasov@i.ua>
2020-06-30 10:20:20 +0300

    Updates

---
+++ precache-manifest.f364a15f1495c9e43c25dc831706df22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bb72160691137232405f0069cf0589",
+    "url": "/react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "db6d4ab8eee0f06aef3d",
+    "url": "/react-hw-03-phonebook/static/js/2.de5ff238.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-03-phonebook/static/js/2.de5ff238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bfc2abfdeb7f4b8353",
+    "url": "/react-hw-03-phonebook/static/js/main.41b18636.chunk.js"
+  },
+  {
+    "revision": "fa5a2918c22e89e0a76d",
+    "url": "/react-hw-03-phonebook/static/js/runtime-main.74c3a849.js"
+  }
+]);
